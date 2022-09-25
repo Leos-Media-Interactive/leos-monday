77edcb08bd4d21336a7bfad208fd1397d4f93f22
Maxim Fol <max.folko@gmail.com>
2022-09-25 12:04:28 +0300

    logger
    logger

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,10 +29,21 @@ Route::middleware('auth:sanctum')->get('/call', function (Request $request) {
 Route::get('/call', function (Request $request){
     $token = config('app.api_token');
     $request_token = $request->token;
+    $phone = $request->phone;
+    $target = $request->target;
 
     if($token !== $request_token){
         return response('No No No', 401);
     }else{
+
+
+
+//        $response = Http::post('https://api.voicenter.com/ForwardDialer/click2call.aspx', [
+//            'phone' => $phone,
+//            'target' => $target,
+//        ]);
+
+
         return response()->json([
             'app_token' => $token,
             'you_token' => $request_token,
